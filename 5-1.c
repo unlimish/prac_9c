f1799d43832f6a05b8b2a07f265a8ee95fbd1756
unlimish <mail@unlimish.xyz>
2020-07-21 19:54:36 +0900

    Update: increment/decrement

--- 5-1.c
+++ 5-1.c
@@ -1,17 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int left;
+    /*     int left;
     int right;
     left = 10;
     right = 3;
-    
+
     printf("%d\n",left + right);
     printf("%d\n",left - right);
     printf("%d\n",left * right);
     printf("%d\n",left / right);
     printf("%d\n",left % right);
-
+ */
+    int value;
+    value = 10;
+    value += 30;
+    printf("%d\n", value);
+    value++;
+    printf("%d\n", value);
+    value--;
+    printf("%d\n", value);
     return 0;
 }
