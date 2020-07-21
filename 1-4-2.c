edf344cb5c7429a2bd4b3057fac2f5220fe858a8
unlimish <mail@unlimish.xyz>
2020-07-21 19:48:49 +0900

    Add: declaration variables

--- 1-4-2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-    printf("%d\n",100+200);
-    return 0;
-}
---
+++ 4-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%d\n",100 + 200);
+    printf("%d\n",10 - 3);
+    printf("%d\n",10 * 3);
+    printf("%d\n",10 / 3);
+    printf("%d\n",10 % 3);
+    printf("%d\n", (1 + 100) * 100 / 2);
+
+    return 0;
+}
