6b686756fb2abe3534b214f1572692781ce5780b
unlimish <mail@unlimish.xyz>
2020-07-24 12:06:56 +0900

    real num digit alignment

--- 5-4.c
+++ 5-4.c
@@ -3,10 +3,15 @@
 int main(void)
 {
     int a = 1000, b = 500, c = 3;
+    double pi = 3.14159;
 
-    printf("Aは %05d です。\n", a);
-    printf("Bは %05d です。\n", b);
-    printf("Cは %05d です。\n", c);
+    //%全体桁数.小数桁数f
+
+    printf("Aは %6d です。\n", a);
+    printf("Bは %6d です。\n", b);
+    printf("Cは %6d です。\n", c);
+
+    printf("Piは %6.2f です。\n", pi);
 
     return 0;
 }
