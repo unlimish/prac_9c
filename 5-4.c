3fde825d632e40423b6e342c539d2f3372a600d6
unlimish <mail@unlimish.xyz>
2020-07-24 12:02:38 +0900

    Digit alignment

---
+++ 5-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1000, b = 500, c = 3;
+
+    printf("Aは %5d です。\n", a);
+    printf("Bは %5d です。\n", b);
+    printf("Cは %5d です。\n", c);
+
+    return 0;
+}
