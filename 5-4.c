1fc9100cd22329e4f4693fdb3d3e4cbfee02978c
unlimish <mail@unlimish.xyz>
2020-07-24 12:03:09 +0900

    zero digit

--- 5-4.c
+++ 5-4.c
@@ -4,9 +4,9 @@ int main(void)
 {
     int a = 1000, b = 500, c = 3;
 
-    printf("Aは %5d です。\n", a);
-    printf("Bは %5d です。\n", b);
-    printf("Cは %5d です。\n", c);
+    printf("Aは %05d です。\n", a);
+    printf("Bは %05d です。\n", b);
+    printf("Cは %05d です。\n", c);
 
     return 0;
 }
