85d17f172115ac8b59a45773d29220b0fea7ec3e
unlimish <mail@unlimish.xyz>
2020-07-25 17:40:44 +0900

    IF with block style writing

--- 7-3.c
+++ 7-3.c
@@ -4,7 +4,10 @@ int main(void){
     int score;
     printf("Enter score");
     scanf("%d",&score);
-    if(score > 100) score = 100;
+    if(score > 100){
+        printf("Your entered score is bigger than 100 so I'll edit to 100.\n");
+        score = 100;
+    }
     printf("Your score is %d",score);
     return 0;
 }
