ca9d6dffbface3684013ef467f8ef1cd5aadc068
unlimish <mail@unlimish.xyz>
2020-08-05 19:43:55 +0900

    Refactoring

--- 08-01.c
+++ 08-01.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int num;
-    scanf("%d", &num);
-    if (num == 10)
-    {
-        printf("Imput num is 10.\n");
-    }
-    else
-    {
-        printf("Imput num is not 10.\n");
-    }
+int main(void) {
+  int num;
+  scanf("%d", &num);
+  if (num == 10) {
+    printf("Imput num is 10.\n");
+  } else {
+    printf("Imput num is not 10.\n");
+  }
 
-    return 0;
+  return 0;
 }
--- 08-02.c
+++ 08-02.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-    int age;
-    printf("Age:");
-    scanf("%d",&age);
-    if(age <= 3) printf("幼児:無料\n");
-    if(age >= 4 && age <= 12) printf("子供:250円\n");
-    if(age >= 13) printf("大人:400円\n");
-    return 0;
+int main(void) {
+  int age;
+  printf("Age:");
+  scanf("%d", &age);
+  if (age <= 3) {
+    printf("幼児:無料\n");
+  } else {
+    if (age <= 12) {
+      printf("子供:250円\n");
+    } else {
+      printf("大人:400円\n");
+    }
+  }
+  return 0;
 }
