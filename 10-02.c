dd429549f29929e3aa6582d78107d4ef4a4b169b
unlimish <mail@unlimish.xyz>
2020-08-07 17:11:35 +0900

    Surface area of circle with do/while

--- 10-02.c
+++ 10-02.c
@@ -4,13 +4,11 @@ int main(void) {
   int r;
   double s;
 
-  printf("半径:");
-  scanf("%d", &r);
-
-  while (r < 0) {
+  do {
     printf("半径:");
     scanf("%d", &r);
-  }
+  } while ( r < 0);
+  
 
   s = r * r * 3.14;
   printf("面積は %f です。\n", s);
