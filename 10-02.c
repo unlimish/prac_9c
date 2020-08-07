1867498803b8b7053ac8a2a0b5aa45eefd29ddca
unlimish <mail@unlimish.xyz>
2020-08-07 17:10:24 +0900

    Surface area of circle with while

--- 10-02.c
+++ 10-02.c
@@ -3,15 +3,17 @@
 int main(void) {
   int r;
   double s;
+
   printf("半径:");
   scanf("%d", &r);
 
-  if (r < 0) {
-    printf("半径は負の値にはなりません。\n");
-  } else {
-    s = r * r * 3.14;
-    printf("面積は %f です。\n", s);
+  while (r < 0) {
+    printf("半径:");
+    scanf("%d", &r);
   }
+
+  s = r * r * 3.14;
+  printf("面積は %f です。\n", s);
 
   return 0;
 }
