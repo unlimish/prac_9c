4bd5736a85f5b51b7002e1a00b65eb843151e325
unlimish <mail@unlimish.xyz>
2020-08-10 13:18:34 +0900

    Multiple arguments

--- 11-02.c
+++ 11-02.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int sum(int); //プロトタイプ宣言
+int sum(int, int);  //プロトタイプ宣言
 
 int main(void) {
-  sum(50);
+  sum(50, 100);
   return 0;
 }
 
-int sum(int max){
-  printf("%d\n", (1 + max) * max / 2);
+int sum(int min, int max) {
+  printf("%d\n", (min + max) * (max - min + 1) / 2);
   return 0;
 }
