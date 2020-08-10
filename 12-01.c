be6a904b9516408ca776985ad0b9dcee2e268cfd
unlimish <mail@unlimish.xyz>
2020-08-10 18:01:30 +0900

    Scope is in the block

--- 12-01.c
+++ 12-01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int countcunc(void);
+/* int countcunc(void);
 
 int main(void) {
   countcunc();
@@ -14,4 +14,22 @@ int countcunc(void) {
   count++;
   printf("%d\n", count);
   return count;
+} */
+
+int main(void) {
+  int value1 = 10;
+  int value2 = 20;
+  printf("1:value1 %d\n", value1);
+  printf("1:value2 %d\n", value2);
+  {
+    int value1;
+    value1 = 30;
+    value2 = 40;
+    printf("2:value1 %d\n", value1);
+    printf("2:value2 %d\n", value2);
+  }
+  printf("3:value1 %d\n", value1);
+  printf("3:value2 %d\n", value2);
+
+  return 0;
 }
