e2072e9a66a433d75e8ac30bde85f48af4b99efa
unlimish <14168376+unlimish@users.noreply.github.com>
2020-08-14 20:31:25 +0900

    strcpy to char string

---
+++ 14-02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  char str[6] = {'M', 'A', 'R', 'I', 'O'};
+  printf("%s\n", str);
+  return 0;
+}
---
+++ 14-03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+ /*  char str[] = "145";
+  int number = atoi(str);
+  printf("%d\n", number); */
+
+char str[10];
+strcpy(str, "MARIO");
+printf("%s\n", str);
+
+  return 0;
+}
